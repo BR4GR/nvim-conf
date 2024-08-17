a471b2d22685bb165f256940630d5f094043473b
BR4GER <benjamin.wuermli@students.fhnw.ch>
2024-08-17 11:31:36 +0200

    use pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 
 vim.opt.clipboard = "unnamedplus"
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
